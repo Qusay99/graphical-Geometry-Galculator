0ced16f3312b34950e4d5638e9d21a5c60ab63d1
qusay99 <qusay.hamid9@gmail.com>
2022-02-18 15:04:31 +0100

    delete all files of the old template

--- README.md
+++
@@ -1,3 +0,0 @@
-# graphical-GeometryGalculator
-## Helpful links for further development:
-Svelte-Router: https://github.com/ItalyPaleAle/svelte-spa-router <br>
--- src/routes.js
+++
@@ -1,7 +0,0 @@
-// import Home from './components/Home.svelte';
-import Home from './components/GeomCalcu.svelte'
-
-export default {
-    //Exaxt path
-    '/': Home,
-}
